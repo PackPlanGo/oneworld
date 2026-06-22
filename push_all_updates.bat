7f1dfa7b3a6d9632d70d60cb84078b6ba6cb17bf
PackPlanGo <jamawe95@gmail.com>
2026-06-23 00:13:43 +1000

    deploy: push all pending HTML updates

--- push_all_updates.bat
+++ push_all_updates.bat
@@ -1,5 +1,6 @@
 @echo off
 cd /d "C:\Users\jamaw\OneDrive\Dokumente\Claude\Projects\Packplango\GitHub\oneworld"
+git pull origin main
 git add -A
 git commit -m "deploy: push all pending HTML updates"
 git push origin main
